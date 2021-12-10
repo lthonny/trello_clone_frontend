3c31b6a1f1394c05d62fd90759d61c34fc6978e2
lthonny <thonny02@mail.ru>
2021-12-10 19:25:29 +0300

    fix jasmine test

---
+++ src/app/admin/board-page/components/add-board/add-board.component.spec.ts
@@ -0,0 +1,27 @@
+// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+//
+// import {AddBoardComponent} from './add-board.component';
+// import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+//
+// describe('AddBoardComponent', () => {
+//   let component: AddBoardComponent;
+//   let fixture: ComponentFixture<AddBoardComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       imports: [MatDialogModule],
+//       declarations: [AddBoardComponent],
+//     })
+//       .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AddBoardComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
